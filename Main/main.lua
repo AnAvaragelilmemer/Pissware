cf476a53bae66f5c0f79e6cd499fbb283af3129d
AnAvaragelilmemer <116945939+AnAvaragelilmemer@users.noreply.github.com>
2023-03-20 21:07:27 +0800

    Update main.lua

--- Main/main.lua
+++ Main/main.lua
@@ -222,7 +222,7 @@ end
 else
 	for i,v in pairs(game:GetService("Players"):GetPlayers()) do
 	if v.Name ~= lplr.Name and (v.Team ~= lplr.Team or (not getgenv().teamcheck)) then
-	v.Character.HumanoidRootPart.Transparency = 0.5
+	v.Character.HumanoidRootPart.Transparency = 1
 	v.Character.HumanoidRootPart.Color = Color3.new(255,255,255)
 	v.Character.HumanoidRootPart.Size = Vector3.new(2,2,2)
 	v.Character.HumanoidRootPart.Material = "Plastic"
